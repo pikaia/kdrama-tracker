88c42a59d3bb5770e9c9cf86014fc295d61bf58f
pikaia <chriskslee@gmail.com>
2022-11-09 22:23:58 -0500

    Hide list items so hidden episodes do not show a placeholder

--- background.js
+++ background.js
@@ -1,17 +1,10 @@
-function checkLinks() {
-  console.log("Check Links called");
-  // Limit to the left content div
-  // Filter for links to episodes which already are dubbed in English (Not RAW)
-  console.table([...document.querySelector("div.tab-content.left-tab-1.selected")
-    .querySelectorAll("a")].filter(l => l.href.includes('episode') && l.getElementsByClassName("ep RAW").length == 0).map(l=> l.href));
-}
+console.log("kdrama background script running");
 
-chrome.action.onClicked.addListener((tab) => {
-  console.error("Krama Tracker Loaded");
-  if (!tab.url.includes("chrome://")) {
-    chrome.scripting.executeScript({
-      target: { tabId: tab.id },
-      function: checkLinks
-    });
-  }
-});
+chrome.action.onClicked.addListener(buttonClicked);
+
+function buttonClicked(tab) {
+  console.log("button clicked: " + tab);
+  chrome.tabs.sendMessage(tab.id, {
+    txt: "Kdrama content script go",
+  });
+}
--- content.js
+++ content.js
@@ -7,6 +7,16 @@ chrome.runtime.onMessage.addListener(gotMessage);
 
 function gotMessage(message, sender, sendResponse) {
   console.log("Got Message function: " + message);
+
+  // Hide elements to shrink display
+  document.getElementsByClassName("logo")[0].style.display = "none";
+  document.getElementsByClassName("char-list")[0].style.display = "none";
+  document.getElementsByClassName("menu_top")[0].style.display = "none";
+  document.getElementsByClassName("slide_mobilde")[0].style.display = "none";
+  document.getElementsByClassName("content-right")[0].style.display = "none";
+  document.getElementsByClassName("tab")[0].style.display = "none";
+  document.getElementsByClassName("block-tab")[1].style.display = "none";
+
   // Limit to the left content div
   // Filter for links to episodes which already are dubbed in English (Not RAW)
   const episodes = [
@@ -18,7 +28,8 @@ function gotMessage(message, sender, sendResponse) {
     if (anchor.href.includes("episode")) {
       console.log("Checking link: " + anchor);
       if (anchor.getElementsByClassName("ep RAW").length > 0) {
-        anchor.style.visibility = "hidden";
+        // Hide parent so that hidden nodes are not displayed.
+        anchor.parentNode.style.display = "none";
       }
     }
   });
